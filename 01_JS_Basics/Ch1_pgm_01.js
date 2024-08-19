af7f7fa1990e03aae5fc2afa0ee6b2a169992492
Suganthi-A <asuganthi387@gmail.com>
2024-08-19 21:21:46 +0530

    JS Basics

---
+++ 01_JS_Basics/Ch1_pgm_01.js
@@ -0,0 +1,22 @@
+// Using console.log to display information
+
+console.log("Hello Everyone");
+
+console.log("How are you");
+
+console.log("Javascript is very useful");
+
+
+/* Further Adventures
+ *
+ * 1) Change the text between the parentheses.
+ *
+ * 2) Click Run on the console to run the program again.
+ *
+ * 3) Add your own console.log statement
+ *    to the program, so there are two.
+ *    Don't forget the semicolon.
+ *
+ * 4) Click Run.
+ *
+ */
