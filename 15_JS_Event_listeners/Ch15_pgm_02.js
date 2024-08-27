baadb051758d4c84b26682c1c0fc8bdedc8eafbd
Suganthi-A <asuganthi387@gmail.com>
2024-08-27 08:16:57 +0530

    Event Listeners

---
+++ 15_JS_Event_listeners/Ch15_pgm_02.js
@@ -0,0 +1,11 @@
+
+// Generating the keyboard code code using even listener. 
+
+// The image below.
+const keyDisplay = document.getElementById('key-display');
+
+// Add event listener to capture key presses
+document.addEventListener('keydown', event => {
+    keyDisplay.textContent = `Key Code: ${event.keyCode}`;
+});
+// https://github.com/Asabeneh/30-Days-Of-JavaScript/raw/master/images/projects/dom_min_project_keycode_day_3.2.gif
